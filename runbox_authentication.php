beb69dcac63633cae2c2d7e3e00f5e1fc9f462fc
Fetlock <fetlock@users.noreply.github.com>
2016-01-22 23:16:42 +0000

    Initial commit

---
+++ runbox_authentication.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Runbox Authentication
+ *
+ * Allow connection to Runbox IMAP and SMTP servers by replacing the 'at' symbol
+ * with a 'percent' symbol in the username at connection time.
+ *
+ * Intentionally doesn't hook into user_create in order to preserve a friendly
+ * looking email address in the user interface.
+ *
+ * @version 0.1
+ * @author Fetlock
+ * @url https://github.com/fetlock/roundcube-plugin-runbox-authentication
+ */
+class runbox_authentication extends rcube_plugin
+{
+  public $task = 'login|mail';
+  private $rc;
+  
+  function init()
+  {
+    $this->rc = rcube::get_instance();
+    $this->add_hook('authenticate', array($this, 'authentication'));
+    $this->add_hook('smtp_connect', array($this, 'smtp_connection'));
+  }
+  
+  function authentication($args)
+  {
+    return array('user' => str_replace('@', '%', $args['user']));
+  }
+  
+  function smtp_connection($args)
+  {
+    return array('smtp_user' => str_replace('@', '%', $this->rc->user->get_username()));
+  }
+}
